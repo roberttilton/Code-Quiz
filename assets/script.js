b51d96ab5da0df69ab04f0e02b3df582aef66fe7
Robert Tilton <robtilton2086@gmail.com>
2021-02-10 16:26:32 -0500

    filling out javascript and struggling along with the functions

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,28 @@
 var timeLimit = 90;
 var score = 0;
-var answers = document.getElementById("quiz-holder")
+var question = document.getElementById("question");
+var answers = document.getElementById("");
+var time = document.getElementById("time");
+var start = document.getElementById("start");
+var ans1 = document.getElementById("ans1");
+var ans2 = document.getElementById("ans2");
+var ans3 = document.getElementById("ans3");
+var ans4 =document.getElementById("ans4");
 
-function countdown() {
+function tickTock() {
+    setInterval(function countdown() {
     timeLimit --
-    displayTimer.textContent = "Time Remaining: " + timeLimit
+    time.textContent = timeLimit
     if(timeLeft <= 0 ) {
         console.log("Game Over");
         clearInterval(timer);
     }
+}, 1000);
 };
 
-var questionsApp = {
+function qS() {
+    var questionsApp = {
+    // using the property "questions" to build out my questions for the quiz, which have taken a decidedly Lynchian turn
     questions:[
     {q:"What film of David Lynch's won the Palme d' Or at the Cannes Film Festival?",
      inputs:['Blue Velvet', 'Wild at Heart', 'Inland Empire', 'Twin Peaks: Fire Walk With Me'],
@@ -21,18 +32,34 @@ var questionsApp = {
     inputs:['Laura Dern', 'Miguel Ferrer', 'Kyle MacLachlan', 'Ray Wise'],
     answer:2
     },
-    {q:"In what episode of Twin Peaks: The Return does Cooper return fully from the Black Lodge-induced dream state he is in?",
+    {q:"In what episode of Twin Peaks: The Return does Agent Cooper return fully from the Black Lodge-induced dream state he is in?",
     inputs:['Part 16', 'Part 5', 'Part 7', 'Part 10'],
     answer:0
     },
-    {q:"What film of David Lynch's won the Palme d' Or at the Cannes Film Festival?",
-    inputs:['Blue Velvet', 'Wild at Heart', 'Inland Empire', 'Twin Peaks: Fire Walk With Me'],
-    answer:1
+    {q:"Who co-wrote Lost Highway and Wild at Heart with David Lynch?",
+    inputs:['Mark Frost', 'Stephen King', 'Barry Gifford', 'Andrew Kevin Walker'],
+    answer:2
     },
-    {q:"What ",
-    inputs:['Blue Velvet', 'Wild at Heart', 'Inland Empire', 'Twin Peaks: Fire Walk With Me'],
-    answer:1
+    {q:"What famous film franchise were two of the great David's of film, Cronenberg and Lynch, both at one point considered to helm entries in?",
+    inputs:['Indiana Jones', 'Jaws', 'Nightmare on Elm Street', 'Star Wars'],
+    answer:3
     }
-    
-]
+],
+index:0,
+load:function() {
+    if (index<=questions.length-1) {
+        question.innerHTML=index+ 1 + ". " +questions[index].q;
+        ans1.textContent=this.questions[this.index].inputs[0];
+        ans2.textContent=this.questions[this.index].inputs[1];
+        ans3.textContent=this.questions[this.index].inputs[2];
+        ans4.textContent=this.questions[this.index].inputs[3];
+    } else {
+    alert("Quiz Completed!")
+    }
 }
+}
+}
+
+
+start.addEventListener("click", tickTock);
+start.addEventListener("click", qS);
